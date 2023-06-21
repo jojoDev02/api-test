2e0aa6488e1167b532188b7bec8605491c751239
Maria Jordana <jordana-cavalcante@outlook.com>
2023-06-21 20:17:51 -0300

    ✨adding logic in endpoints customer

--- controllers/customer/customer_routes.py
+++ controllers/customer/customer_routes.py
@@ -1,9 +1,11 @@
 from flask import Blueprint, jsonify, request
+from models.address import AddressCustomer
 
 from models.customer import Customer
 from db.database import db_session
 
 customer_bp =  Blueprint("customer", __name__)
+
 
 @customer_bp.route("/customers", methods = ['GET'])
 def get_customers():
@@ -22,11 +24,24 @@ def get_customers():
 
 @customer_bp.route("/customers/<int:id_customer>", methods = ['GET'])
 def get_customer_by_id(id_customer):
-    pass
+    customer = Customer.query.get(id_customer)
+
+    if not customer:
+        return jsonify({'error': 'Customer not found'}), 404
+
+    return jsonify({
+        'customer': {
+            'id': customer.id,
+            'cpf': customer.cpf,
+            'name': customer.name,
+            'phone_number': customer.phone_number
+        }
+    }), 200
 
 @customer_bp.route("/customers", methods = ['POST'])
 def create_customer():
     data = request.json
+
     email = data.get('email')
     password = data.get('password')
     cpf = data.get('cpf')
@@ -52,16 +67,127 @@ def create_customer():
             'type': customer.user_type
         }
     }), 201
-
-    
-
+   
 @customer_bp.route("/customers", methods = ['PUT'])
 def update_customer():
     pass
 
-@customer_bp.route("/customers", methods = ['DELETE'])
-def delete_customer():
-    pass
+@customer_bp.route("/customers/<customer_id>", methods = ['DELETE'])
+def delete_customer(customer_id):
+    customer = Customer.query.get(customer_id)
+
+    if not customer:
+        return jsonify({'error': 'Customer not found'}), 404
+
+    db_session.delete(customer)
+    db_session.commit()
+
+    return jsonify({'message': 'Customer deleted successfully'}), 200
+
+@customer_bp.route("/customers/<int:customer_id>/addresses/<int:address_id>", methods = ['GET'])
+def get_address(id_customer, id_address):
+
+    customer = Customer.query.get(id_customer)
+
+    if not customer:
+        return jsonify({'error':'Customer not found'}), 404
+    
+    address = AddressCustomer.query.filter_by(customer_id=id_customer, id=id_address).first()
+
+    if not address:
+        return jsonify({'error':'Address not found'}), 404
+    
+    address_data = {
+        'id': address.id,
+        'nickname': address.nickname,
+        'cep': address.cep,
+        'street': address.street,
+        'state': address.state,
+        'city': address.city,
+        'neighborhood': address.neighborhood,
+        'number': address.number,
+        'complement': address.complement
+    }
+
+    return jsonify(address_data), 200
+
+
+@customer_bp.route("/customers/<int:customer_id>/addresses", methods = ['GET'])
+def get_addresses(customer_id):
+
+    customer = Customer.query.get(customer_id)
+
+    if not customer:
+        return jsonify({'error': 'Customer not found'}), 404
+
+    addresses = AddressCustomer.query.filter_by(customer_id=customer_id).all()
+
+    addresses_data = []
+    for address in addresses:
+        address_data = {
+            'id': address.id,
+            'nickname': address.nickname,
+            'cep': address.cep,
+            'street': address.street,
+            'state': address.state,
+            'city': address.city,
+            'neighborhood': address.neighborhood,
+            'number': address.number,
+            'complement': address.complement
+        }
+        addresses_data.append(address_data)
+
+    return jsonify(addresses_data), 200
+
+@customer_bp.route("/customers/<int:customer_id>/addresses", methods = ['POST'])
+def create_address(customer_id):
+    customer = Customer.query.get(customer_id)
+
+    if not customer:
+        return jsonify({'error': 'Customer not found'}), 404
+    
+    data = request.json
+    nickname = data.get('nickname')
+    cep = data.get('cep')
+    street = data.get('street')
+    state = data.get('state')
+    city = data.get('city')
+    neighborhood = data.get('neighborhood')
+    number = data.get('number')
+    complement = data.get('complement')
+
+    address = AddressCustomer(
+        customer_id=customer_id,
+        nickname=nickname,
+        cep=cep,
+        street=street,
+        state=state,
+        city=city,
+        neighborhood=neighborhood,
+        number=number,
+        complement=complement
+    )
+
+    customer.addresses.append(address)
+    db_session.add(address)
+    db_session.commit
+
+
+    address_data = {
+        'customer_id': address.customer_id ,
+        'id': address.id_address_customer,
+        'nickname': address.nickname,
+        'cep': address.cep,
+        'street': address.street,
+        'state': address.state,
+        'city': address.city,
+        'neighborhood': address.neighborhood,
+        'number': address.number,
+        'complement': address.complement
+    }
+
+    return jsonify(address_data), 201 
+
 
 @customer_bp.route("/customers/<int:id_customer>/orders", methods =['GET'])
 def get_orders_customer(id_customer):
