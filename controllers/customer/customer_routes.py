8ff777dd4eeb868417b200fbbab4e8656a3c08c1
Maria Jordana <jordana-cavalcante@outlook.com>
2023-07-01 14:39:52 -0300

    🔄refatorando rotas cliente

--- controllers/customer/customer_routes.py
+++ controllers/customer/customer_routes.py
@@ -2,6 +2,7 @@ from flask import Blueprint, jsonify, request
 
 from repositories.customer_repository import CustomerRepository
 from repositories.address_customer_repository import AddressCustomerRepository
+from auth.auth import auth_required
 
 customer_bp =  Blueprint("customer", __name__)
 
@@ -9,7 +10,7 @@ customer_repository = CustomerRepository()
 address_repository = AddressCustomerRepository()
 
 
-@customer_bp.route("/customers", methods = ['GET'])
+@customer_bp.route("/clientes", methods = ['GET'])
 def get_customers():
     
     customers =customer_repository.get_customer_all()
@@ -18,87 +19,61 @@ def get_customers():
         return jsonify({'message': 'No customers found'}), 404
     
     customer_list = [
-        {'id': customer.id, 'cpf': customer.cpf, 'name': customer.name, 'phone_number': customer.phone_number, 'email': customer.email}
+        {'id': customer.id, 'cpf': customer.cpf, 'nome': customer.nome, 'telefone': customer.telefone, 'email': customer.email}
         for customer in customers
     ]
 
-    return jsonify({'customers': customer_list}), 200
+    return jsonify({'clientes': customer_list}), 200
     
 
-@customer_bp.route("/customers/<int:id_customer>", methods = ['GET'])
-def get_customer_by_id(id_customer):
-    customer =customer_repository.get_customer_by_id(id_customer)
+@customer_bp.route("/clientes/<int:cliente_id>", methods = ['GET'])
+def get_customer_by_id(cliente_id):
+    customer =customer_repository.get_customer_by_id(cliente_id)
 
     if not customer:
         return jsonify({'error': 'Customer not found'}), 404
 
-    return jsonify({
-        'customer': {
-            'id': customer.id,
-            'email': customer.email,
-            'cpf': customer.cpf,
-            'name': customer.name,
-            'phone_number': customer.phone_number
-        }
-    }), 200
+    return jsonify(customer.to_dict()),200
 
-@customer_bp.route("/customers", methods = ['POST'])
+@customer_bp.route("/clientes", methods = ['POST'])
 def create_customer():
     data = request.json
 
     email = data.get('email')
-    password = data.get('password')
+    senha = data.get('senha')
     cpf = data.get('cpf')
-    name = data.get('name')
-    phone_number = data.get('phone_number')
+    nome = data.get('nome')
+    telefone = data.get('telefone')
     
-    customer =customer_repository.create_customer(email=email, password=password,cpf=cpf, name=name, phone_number=phone_number)
+    customer =customer_repository.create_customer(email, senha,cpf, nome, telefone)
 
-    return jsonify({
-        'customer':{
-            'id': customer.id,
-            'email': customer.email,
-            'cpf': customer.cpf,
-            'name': customer.name,
-            'phone_number': customer.phone_number,
-            'type': customer.user_type
-            }
-    }), 201
+    return jsonify(customer.to_dict()), 201
    
-@customer_bp.route("/customers/<int:customer_id>", methods = ['PUT'])
-def update_customer(customer_id):
-    customer=customer_repository.get_customer_by_id(customer_id)
+@customer_bp.route("/clientes/<int:cliente_id>", methods = ['PUT'])
+def update_customer(cliente_id):
+    customer=customer_repository.get_customer_by_id(cliente_id)
 
     if not customer:
         return jsonify({'error': 'Customer not found'}), 404
 
     data = request.json
     email = data.get('email')
-    name = data.get('name')
-    phone_number = data.get('phone_number')
+    nome = data.get('nome')
+    telefone = data.get('telefone')
 
-    customer =customer_repository.update_customer(customer,email, name, phone_number)
+    customer =customer_repository.update_customer(customer,email, nome, telefone)
 
-    return jsonify({
-        'customer':{
-            'id': customer.id,
-            'email': customer.email,
-            'cpf': customer.cpf,
-            'name': customer.name,
-            'phone_number': customer.phone_number,
-            'type': customer.user_type
-            }
-    })
+    return jsonify(customer.to_dict())
     
 
-@customer_bp.route("/customers/<customer_id>", methods = ['DELETE'])
-def delete_customer(customer_id):
-    customer=customer_repository.get_customer_by_id(customer_id)
+@customer_bp.route("/clientes/<cliente_id>", methods = ['DELETE'])
+def delete_customer(cliente_id):
+    customer=customer_repository.get_customer_by_id(cliente_id)
 
     if not customer:
         return jsonify({'error': 'Customer not found'}), 404
     
-    customer_repository.delete_customer(customer_id)
+    customer_repository.delete_customer(cliente_id)
 
     return jsonify({'message': 'Customer deleted successfully'}), 200
 
@@ -106,150 +81,83 @@ def delete_customer(customer_id):
 
 #ADDRESS ROUTES
 
-@customer_bp.route("/customers/<int:customer_id>/addresses/<int:address_id>", methods = ['GET'])
-def get_address(customer_id, address_id):
-    customer = customer_repository.get_customer_by_id(customer_id)
+@customer_bp.route("/clientes/<int:cliente_id>/enderecos/<int:endereco_id>", methods = ['GET'])
+def get_address(cliente_id, endereco_id):
+    customer = customer_repository.get_customer_by_id(cliente_id)
 
     if not customer:
         return jsonify({'error': 'Customer not found'}), 404
 
-    address = address_repository.get_address_customer_by_id(address_id)
+    endereco = address_repository.get_address_customer_by_id(endereco_id)
 
-    if not address:
+    if not endereco:
         return jsonify({'error': 'Address not found'}), 404
 
-    return jsonify({
-        'address': {
-            'id': address.id,
-            'customer_id': address.customer_id,
-            'nickname': address.nickname,
-            'cep': address.cep,
-            'street': address.street,
-            'state': address.state,
-            'city': address.city,
-            'neighborhood': address.neighborhood,
-            'number': address.number,
-            'complement': address.complement
-        }
-    })
+    return jsonify(endereco.to_dict())
 
 
-@customer_bp.route("/customers/<int:customer_id>/addresses", methods = ['GET'])
-def get_addresses(customer_id):
-    addresses = address_repository.get_address_customer_all(customer_id)
-    if not addresses:
-        return jsonify({'error': 'No addresses found for the customer'}), 404
+@customer_bp.route("/clientes/<int:cliente_id>/enderecos", methods = ['GET'])
+def get_addresses(cliente_id):
+    enderecos = address_repository.get_address_customer_all(cliente_id)
+    if not enderecos:
+        return jsonify({'error': 'No enderecos found for the customer'}), 404
 
-    address_list = []
-    for address in addresses:
-        address_data = {
-            'id': address.id,
-            'customer_id': address.customer_id,
-            'nickname': address.nickname,
-            'cep': address.cep,
-            'street': address.street,
-            'state': address.state,
-            'city': address.city,
-            'neighborhood': address.neighborhood,
-            'number': address.number,
-            'complement': address.complement
-        }
-        address_list.append(address_data)
+    enderecos_lista = []
+    for endereco in enderecos:
+        endereco_dados = endereco.to_dict()
+        enderecos_lista.append(endereco_dados)
 
-    if not address_list:
-        return jsonify({'message':'Customer has no registered addresses'}),204
+    if not enderecos_lista:
+        return jsonify({'message':'Customer has no registered enderecos'}),204
     
-    return jsonify({'addresses': address_list})
+    return jsonify({'enderecos': enderecos_lista})
 
-@customer_bp.route("/customers/<int:customer_id>/addresses", methods = ['POST'])
-def create_address(customer_id):
-    customer = customer_repository.get_customer_by_id(customer_id)
+@customer_bp.route("/clientes/<int:cliente_id>/enderecos", methods = ['POST'])
+def create_address(cliente_id):
+    customer = customer_repository.get_customer_by_id(cliente_id)
 
     if not customer:
         return jsonify({'error': 'Customer not found.'}),404
     
-    data = request.json
-    address_data = {
-        'customer_id': customer_id,
-        'nickname': data.get('nickname'),
-        'cep': data.get('cep'),
-        'street': data.get('street'),
-        'state': data.get('state'),
-        'city': data.get('city'),
-        'neighborhood': data.get('neighborhood'),
-        'number': data.get('number'),
-        'complement': data.get('complement'),
-    }
-
+  
+    address_data = request.get_json()
+    address_data['cliente_id'] = cliente_id
     address_customer = address_repository.create_address_customer(**address_data)
-    customer.addresses.append(address_customer)
-    return  jsonify({
-        'id': address_customer.id,
-        'nickname': address_customer.nickname,
-        'cep': address_customer.cep,
-        'street': address_customer.street,
-        'state': address_customer.state,
-        'city': address_customer.city,
-        'neighborhood': address_customer.neighborhood,
-        'number': address_customer.number,
-        'complement': address_customer.complement,
-        'customer_id': address_customer.customer_id
-    })
 
-@customer_bp.route("/customers/<int:customer_id>/addresses/<int:address_id>", methods = ['PUT'])
-def update_address(customer_id, address_id):
-    customer = customer_repository.get_customer_by_id(customer_id)
-    address_customer = address_repository.get_address_customer_by_id(address_id)
+    customer.enderecos.append(address_customer)
+    return  jsonify(address_customer.to_dict()),201
+
+@customer_bp.route("/clientes/<int:cliente_id>/enderecos/<int:endereco_id>", methods = ['PUT'])
+def update_address(cliente_id, endereco_id):
+    customer = customer_repository.get_customer_by_id(cliente_id)
+    address_customer = address_repository.get_address_customer_by_id(endereco_id)
     if not customer or not address_customer:
-        return jsonify({'error': 'Customer or address not found'}), 404
-    
-    data = request.json
+        return jsonify({'error': 'Customer or endereco not found'}), 404
 
-    address_data = {
-        'nickname': data.get('nickname'),
-        'cep': data.get('cep'),
-        'street': data.get('street'),
-        'state': data.get('state'),
-        'city': data.get('city'),
-        'neighborhood': data.get('neighborhood'),
-        'number': data.get('number'),
-        'complement': data.get('complement'),
-    }
-    
+    address_data = request.get_json()
     address_repository.update_address_customer(address_customer, **address_data)
 
-    return  jsonify({
-        'id': address_customer.id,
-        'nickname': address_customer.nickname,
-        'cep': address_customer.cep,
-        'street': address_customer.street,
-        'state': address_customer.state,
-        'city': address_customer.city,
-        'neighborhood': address_customer.neighborhood,
-        'number': address_customer.number,
-        'complement': address_customer.complement,
-        'customer_id': address_customer.customer_id
-    })
+    return  jsonify(address_customer.to_dict())
 
 
 
-@customer_bp.route("/customers/<int:customer_id>/addresses/<int:address_id>", methods = ['DELETE'])
-def delete_address(customer_id, address_id):
+@customer_bp.route("/clientes/<int:cliente_id>/enderecos/<int:endereco_id>", methods = ['DELETE'])
+def delete_address(cliente_id, endereco_id):
 
-    customer = customer_repository.get_customer_by_id(customer_id)
+    customer = customer_repository.get_customer_by_id(cliente_id)
 
     if not customer:
         return jsonify({'error': 'Customer not found'}), 404
 
-    address = address_repository.get_address_customer_by_id(address_id)
+    endereco = address_repository.get_address_customer_by_id(endereco_id)
 
-    if not address:
+    if not endereco:
         return jsonify({'error': 'Address not found'}), 404
 
-    if address.customer_id != customer_id:
+    if endereco.cliente_id!= cliente_id:
         return jsonify({'error': 'Address does not belong to the customer'}), 400
 
-    address_repository.delete_address_customer(address)
+    address_repository.delete_address_customer(endereco)
 
     return jsonify({'message': 'Address deleted successfully'})
 
@@ -257,16 +165,16 @@ def delete_address(customer_id, address_id):
 
 
 
-@customer_bp.route("/customers/<int:id_customer>/orders", methods =['GET'])
-def get_orders_customer(id_customer):
+@customer_bp.route("/clientes/<int:cliente_id>/pedidos", methods =['GET'])
+def get_orders_customer(cliente_id):
     pass
 
-@customer_bp.route("/customers/<int:id_customer>/orders", methods =['POST'])
-def create_orders_customer(id_customer):
+@customer_bp.route("/clientes/<int:cliente_id>/pedidos", methods =['POST'])
+def create_orders_customer(cliente_id):
     pass
 
-@customer_bp.route("/customers/<int:id_customer>/orders/<int:id_order>", methods =['DELETE'])
-def delete_orders_customer(id_customer, id_order):
+@customer_bp.route("/clientes/<int:cliente_id>/pedidos/<int:pedido_id>", methods =['DELETE'])
+def delete_orders_customer(cliente_id, pedido_id):
     pass
 
 
