ecea490566738b385276fdbcc4ec72a61fd9a185
Maria Jordana <jordana-cavalcante@outlook.com>
2023-07-01 21:28:15 -0300

    criando cupom_repository

---
+++ repositories/cupom_repository.py
@@ -0,0 +1,27 @@
+from flask import jsonify
+from models.cupom import Cupom
+from db.database import db_session
+class CupomRepository:
+
+    def create_cupom(self, **dados):
+        cupom = Cupom(**dados)
+        db_session.add(cupom)
+        db_session.commit()
+        return cupom
+
+    def get_cupom_by_id(self, cupom_id):
+        return db_session.query(Cupom).get(cupom_id)
+        
+    def update_cupom(self, cupom, **dados):
+        if cupom:
+           for key, value in dados.items():
+               setattr(cupom, key, value)
+        db_session.commit()
+        return cupom
+
+
+    def delete_cupom(self, cupom):
+        db_session.delete(cupom)
+        db_session.commit()
+
+    
