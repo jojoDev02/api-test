72249bc7a6f3dcdea207614010d22d43d7cf7c9c
Maria Jordana <jordana-cavalcante@outlook.com>
2023-07-04 16:06:52 -0300

    ✨Listar favoritos

--- controllers/customer/favorite_routes.py
+++ controllers/customer/favorite_routes.py
@@ -1,16 +1,52 @@
 from flask import Blueprint, jsonify, request
+from repositories.customer_repository import CustomerRepository
+
+from repositories.favorite_repository import FavoriteRepository
+from repositories.item_repository import ItemRestaurantRepository
 
 
-favorite_bp =  Blueprint("favorite", __name__)
+favorite_bp =  Blueprint("favorite", __name__, url_prefix='/clientes')
+favorite_repo = FavoriteRepository()
+customer_repo = CustomerRepository()
+item_repo = ItemRestaurantRepository()
 
-@favorite_bp.route('/clientes/<int:cliente_id>/favoritos', methods=['GET'])
+@favorite_bp.route('/<int:cliente_id>/favoritos', methods=['GET'])
 def get_favorites(cliente_id):
-    pass
+    cliente = customer_repo.get_customer_by_id(cliente_id)
+    favoritos = favorite_repo.get_favorites(cliente)
 
-@favorite_bp.route('/clientes/<int:cliente_id>/favoritos/<int:item_id>', methods=['POST'])
+    favoritos=[favorito.to_dict() for favorito in favoritos]
+    return jsonify(favoritos)
+
+@favorite_bp.route('/<int:cliente_id>/favoritos/<int:item_id>', methods=['POST'])
 def add_favorite(cliente_id,item_id):
-    pass
+    cliente = customer_repo.get_customer_by_id(cliente_id)
+    item = item_repo.get_item_restaurant_by_id(item_id)
 
-@favorite_bp.route('/clientes/<int:cliente_id>/favoritos/<int:item_id>', methods=['DELETE'])
+    if not cliente:
+        return jsonify({'message': 'Cliente não encontrado.'}), 404
+    if not item:
+        return jsonify({'message': 'Item não encontrado.'}), 404
+    
+    favorito = favorite_repo.add_favorite(cliente, item)
+    return jsonify({
+        'cliente_id': favorito.cliente_id,
+        'item_id' : favorito.item_id
+    }),200
+
+@favorite_bp.route('/<int:cliente_id>/favoritos/<int:item_id>', methods=['DELETE'])
 def delete_favorite(cliente_id,item_id):
-    pass
+    cliente = customer_repo.get_customer_by_id(cliente_id)
+    item = item_repo.get_item_restaurant_by_id(item_id)
+
+    if not cliente:
+        return jsonify({'message': 'Cliente não encontrado.'}), 404
+    if not item:
+        return jsonify({'message': 'Item não encontrado.'}), 404
+
+    favorito = favorite_repo.get_favorite(cliente_id, item_id)
+    if not favorito:
+        return jsonify({'message': 'Favorito não encontrado.'}), 404
+
+    favorite_repo.remove_favorite(favorito)
+    return jsonify({'message': 'Favorito excluído com sucesso.'}),200
--- repositories/favorite_repository.py
+++ repositories/favorite_repository.py
@@ -6,10 +6,10 @@ class FavoriteRepository:
 
     def get_favorites(self, cliente):
         if cliente:
-            return db_session.query(ItemRestaurant).join(Favorite).filter(Favorite.cliente_id == cliente.id).all()
-    
+           return cliente.favoritos
+            
     def get_favorite(self, cliente_id, item_id):
-        return db_session.query(Favorite).filter(Favorite.cliente_id == cliente_id, Favorite.item_id == item_id).first()
+        return db_session.query(ItemRestaurant).filter(Favorite.cliente_id == cliente_id, Favorite.item_id == item_id).first()
     
     def add_favorite(self, cliente, item):
         favorito = Favorite(cliente_id = cliente.id, item_id= item.id)
